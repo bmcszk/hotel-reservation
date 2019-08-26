60981f434b3333b45aa38740d26997b3049e9580
Błażej Maciaszek <bmaciaszek@oakfusion.pl>
2019-08-26 17:08:29 +0200

    Spring Cloud

--- src/main/java/blazesoft/hotelreservation/HotelReservationApplication.java
+++ src/main/java/blazesoft/hotelreservation/HotelReservationApplication.java
@@ -3,9 +3,13 @@ package blazesoft.hotelreservation;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
 public class HotelReservationApplication {
 
 	public static void main(String[] args) {
